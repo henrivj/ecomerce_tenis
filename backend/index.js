c72e4aa4072ba68925549d5aeda28681451a45ff
henrivj <henrique_justus@estudante.sesisenai.org.br>
2026-05-25 09:55:07 -0300

    mudei mais uns ngc

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,15 @@ app.use(cors());
 app.post('/usuario', usuarioController.cadastrar);
 app.get('/usuarios', usuarioController.listar);
 app.get('/usuario/:id', usuarioController.consultarId);
-app.get('/usuario/:nome', usuarioController.consultarNome);
+app.get('/usuario/nome/:nome', usuarioController.consultarNome);
 app.put('/usuario/:id', usuarioController.atualizar);
 app.delete('/usuario/:id', usuarioController.deletar);
 
 app.post('/produto', produtoController.cadastrar);
 app.get('/produtos', produtoController.listar);
 app.get('/produto/:id', produtoController.consultarId);
-app.get('/produto/:nome', produtoController.consultarNome);
+app.get('/produto/nome/:nome', produtoController.consultarNome);
+
 app.put('/produto/:id', produtoController.atualizar);
 app.delete('/produto/:id', produtoController.deletar);
 
